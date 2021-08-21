4e71d52f380a948eec3c0251749e29e42a9dc159
DemchukBogdan <demcukbogdan76@gmail.com>
2021-08-21 17:29:00 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+new WOW().init();
+
+$(function () {
+    $('.fotorama').fotorama();
+  });
+
+$('.fotorama').fotorama({
+    spinner: {
+        lines: 13,
+        color: 'rgba(0, 0, 0, .75)'
+    }
+});
+
